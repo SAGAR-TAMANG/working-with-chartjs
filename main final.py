c1b155220545f8bd09810cd5df8d002776fe1c49
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-07-20 05:50:36 +0530

    map created

--- main final.py
+++ main final.py
@@ -15,24 +15,25 @@ for feature in india_states["features"]:
     feature["id"] = feature["properties"]["state_code"]
     state_id_map[feature["properties"]["st_nm"]] = feature["id"]
 
-df = pd.read_csv("india_census.csv")
-df["Density"] = df["Density[a]"].apply(lambda x: int(x.split("/")[0].replace(",", "")))
-df["id"] = df["State or union territory"].apply(lambda x: state_id_map[x])
+df = pd.read_csv("StatewiseTestingDetails.csv")
+# df["Density"] = df["Density[a]"].apply(lambda x: int(x.split("/")[0].replace(",", "")))
+df["TotalSamples"]
+df["id"] = df["State"].apply(lambda x: state_id_map[x])
 
 print(df.head())    
 
-df["DensityScale"] = np.log10(df["Density"])
+df["TotalSamplesScale"] = np.log10(df["TotalSamples"])
 
 fig = px.choropleth_mapbox(
     df,
     locations="id",
     geojson=india_states,
-    color="DensityScale",
-    hover_name="State or union territory",
-    hover_data=["Density"],
-    title="India Population Density",
+    color="TotalSamples",
+    hover_name="State",
+    hover_data=["TotalSamples"],
+    title="India Covid",
     mapbox_style="carto-positron",
-    center={"lat": 28, "lon": 78},
+    center={"lat": 22, "lon": 78},
     zoom=3.5,
     opacity=0.5,
 )
