eb2d34caf4a6930ead6dfbac256fc4652978b565
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-07-26 12:56:37 +0530

    fixed the issues

--- city to state/temp final.py
+++ city to state/temp final.py
@@ -42,29 +42,64 @@ print('THIS IS CITY/STATE Compare after cleaning: \n', cities_compare)
 counter = 0
 counter2 = 0
 counter3 = 0
+counter_page = 0
 
 print("**********************************************")
-df_merged = pd.DataFrame(columns=['City', 'State'])
+# df_merged = pd.DataFrame(columns=['City', 'State'])
 
 for index, row in dt.iterrows():
     try:
         city = row['City'].split()
-        print(city)
+        # print(city)
     except Exception as e:
         counter = counter + 1
         print("EXCEPTION OCCURED IN SPLIT", counter)
         pass
+    
     try:
-        match, score = process.extractOne(city, cities_compare['City'].tolist(), scorer=fuzz.partial_ratio)
-        print("Closest Match:", match, "with Score:", score)
-        
-        # Get the corresponding state from cities_compare
-        state = cities_compare[cities_compare['City'] == match]['State'].iloc[0]
-        print("State:", state)
-        df_merged = pd.merge(naukri, cities_compare, on='City', how='left')
+        city = row['City']
+        print('*******************************************')
+
+        # Initialize variables to keep track of best match and its score
+        best_match = None
+        best_score = 0
+
+        for compare_city in cities_compare['City']:
+            # Perform fuzzy string matching for each city in cities_compare
+            score = fuzz.partial_ratio(city, compare_city)
+
+            # Update best_match and best_score if the current score is better
+            if score > 50:
+                best_match = compare_city
+                best_score = score
+        print("CITY:", city, "| Best Match:", best_match, "| with Score:", best_score)
+
+        matched_rows = cities_compare[cities_compare['City'] == best_match]
+        if not matched_rows.empty:
+            state =  matched_rows['State Name'].iloc[0]
+            print('BEST STATE: ', state)
+            state_df = {
+                'State': state
+            }
+        else:
+            state = None
+            state_df = {
+                'State': [None]
+            }
+            print("STATE NOT FOUND.")
+        # df_merged = df_merged.append({'City': city, 'State': state}, ignore_index=True)
+
+        # print("State:", state)
+        # Add the matched city and state to df_merged
+        try: 
+            naukri.at[counter_page, 'State'] = state
+        except Exception as e:
+            print('Exception occured as: ', e)
+            pass
+        counter_page = counter_page + 1
     except Exception as e:
         counter2 = counter2 + 1
-        print("EXCEPTION OCCCURED IN CITY", counter)
+        print("EXCEPTION OCCCURED IN CITY", counter2)
         pass
-df_merged.to_excel('temp.xlsx')
+naukri.to_excel('temp.xlsx')
 # print('Number of Successful cities to:', counter3)
--- city to state/temp2.py
+++ city to state/temp2.py
@@ -1,30 +1,3 @@
-import pandas as pd
+from rapidfuzz import process, fuzz 
 
-
-dt = naukri
-# print(dt)
-dt['City'] = naukri['City'].apply(cleaning)
-print("THIS IS NAUKRI AFTER THE CLEANING:\n ", dt['City'])
-
-cities_compare = pd.read_csv(r"C:\Users\TAMANG\Documents\GitHub\working-with-chartjs\city to state\Indian Cities Database.csv")
-cities_compare['City'] = cities_compare['City'].apply(cleaning)
-print('THIS IS CITY/STATE Compare after cleaning: \n', cities_compare)
-
-for index, row in dt.iterrows():
-    counter = 0
-    try:
-        city = row['City'].split()
-    except Exception as e:
-        counter = counter + 1
-        print("EXCEPTION OCCURED IN SPLIT")
-        pass
-    try:
-        for i in city:  
-            matching_row = cities_compare.str.contains(i, case=False)
-            dt = pd.concat([dt, matching_row])
-            print(matching_row)
-    except Exception as e:
-        counter = counter + 1
-        print("EXCEPTION OCCCURED", counter)
-        pass
-dt.to_excel('temp.xlsx')
+match, score = process.extractOne('city','city22', scorer=fuzz.partial_ratio)
